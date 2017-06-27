6b303bc0d62e4b7f0d3b2dc04668dc4f1df04048
Mitch S <msamuels+git@usgs.gov>
2017-06-27 14:58:15 -0500

    GH Issues fixed

--- src/app.settings.ts
+++ src/app.settings.ts
@@ -6,7 +6,7 @@ export class APP_SETTINGS {
 
     private static _environment = 'production';
     private static _API_ENDPOINT = 'http://localhost:8000/cbraservices/';
-	// private static _API_ENDPOINT = 'http://cbradev.wim.usgs.gov/cbra/cbraservices/';
+	  // private static _API_ENDPOINT = 'http://cbradev.wim.usgs.gov/cbra/cbraservices/';
 	// private static _API_ENDPOINT = 'https://' + window.location.hostname + '/cbra/cbraservices/';
     public static set environment (env: string) { this._environment = env };
 
