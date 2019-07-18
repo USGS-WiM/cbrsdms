3d2b5a4b5ae7b8b7aaa2313a430aaf2f2695e597
kjacobsen16 <kjacobsen@contractor.usgs.gov>
2019-07-18 11:06:50 -0500

    point to test services

--- src/app.settings.ts
+++ src/app.settings.ts
@@ -5,8 +5,8 @@ import {HttpHeaders} from '@angular/common/http';
 export class APP_SETTINGS {
 
     private static _environment = 'production';
-    private static _API_ENDPOINT = 'http://localhost:8080/cbrsservices/';
-    // private static _API_ENDPOINT = 'http://cbrsdev.wim.usgs.gov/cbrsservices/';
+    // private static _API_ENDPOINT = 'http://localhost:8080/cbrsservices/';
+    private static _API_ENDPOINT = 'http://cbrstest.wim.usgs.gov/cbrsservices/';
     // private static _API_ENDPOINT = 'https://' + window.location.hostname + '/cbrsservices/';
     public static set environment (env: string) { this._environment = env };
 
